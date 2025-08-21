706d4aa6afc709853894cfdb355b681b92dac7f4
guilherme9854 <167765418+guilherme9854@users.noreply.github.com>
2025-08-21 11:40:37 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
 criaCartao(
-'Programação',
+'Geografia',
 'Qual é a capital do Brasil?',
 'Brasília.'
 )
 criaCartao(
-'Programação',
+'Literatura',
 'Quem escreveu "Dom Casmurro"?',
 'Machado de Assis.'
 )
 criaCartao(
-'Programação',
+'Astronomia',
 'Qual é o maior planeta do sistema solar?',
 'Júpiter.'
 )
 criaCartao(
-'Programação',
+'História',
 'Em que ano o Brasil foi descoberto?',
 '1500.'
 )
 criaCartao(
-'Programação',
+'Artes',
 'Quem pintou a Mona Lisa?',
 'Leonardo da Vinci.'
 )
 criaCartao(
-'Programação',
+'Química',
 'Qual é o símbolo químico da água?',
 
 'H₂O.'
