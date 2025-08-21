7bda17519c64ad34360787db074e187e2524b98b
guilherme9854 <167765418+guilherme9854@users.noreply.github.com>
2025-08-21 10:59:22 -0300

    s

---
+++ assets/perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é HTML?&#39;,
+    &#39;O HTML (Linguagem de Marcação de HiperTexto)&#39;
+)
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é CSS?&#39;,
+    &#39;O CSS é uma linguagem de estilização.&#39;
+)
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é JavaScript?&#39;,
+    &#39;O JavaScript é uma linguagem de programação.&#39;    
+)
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é Front-end?&#39;,
+    &#39; O Front-end compreende a parte visual de sites e aplicações.&#39;
+)
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é Back-end?&#39;,
+    &#39;O Back-end compreende tudo aquilo que tem por trás de uma aplicação.&#39;
+)
+criaCartao(
+    &#39;Programação&#39;,
+    &#39;O que é Full Stack?&#39;,
+    &#39;A Full Stack developer é uma pessoa que trabalha tanto com Front-end quanto com Back-
+end.&#39;  
+)
